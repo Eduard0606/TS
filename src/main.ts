cbc1ff0e9172e2d8fbd1bf46370ef3061247b119
Your Name <you@example.com>
2023-08-01 15:35:20 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -721,42 +721,28 @@ const renderDiv = document.getElementById('render')
 if (renderDiv) renderDiv.innerHTML = divElement.getHtml()
 if (renderDiv) renderDiv.innerHTML += pElement.getHtml()
 
+class CssClass{
+name:string
+style:[] = []
 
-// class Clock {
-//    constructor({ template }) {
-//      this.template = template;
-//    }
- 
-//    render() {
-//      let date = new Date();
- 
-//      let hours = date.getHours();
-//      if (hours < 10) hours = '0' + hours;
- 
-//      let mins = date.getMinutes();
-//      if (mins < 10) mins = '0' + mins;
- 
-//      let secs = date.getSeconds();
-//      if (secs < 10) secs = '0' + secs;
- 
-//      let output = this.template
-//        .replace('h', hours)
-//        .replace('m', mins)
-//        .replace('s', secs);
- 
-//      console.log(output);
-//    }
- 
-//    stop() {
-//      clearInterval(this.timer);
-//    }
- 
-//    start() {
-//      this.render();
-//      this.timer = setInterval(() => this.render(), 1000);
-//    }
-//  }
- 
- 
-//  let clock = new Clock({template: 'h:m:s'});
-//  clock.start()
+constructor(name:string){
+   this.name = name
+}
+
+addStyle(){
+   this.style.push()
+}
+
+delStyle(){
+   this.style.pop(style)
+}
+
+getCss() {
+   return `.${this.name}${JSON.stringify(this.style)
+     .replace(/\,/g, "; \n")
+     .replace(/"/g, "")}`;
+ }
+}
+const divElement2 = new CssClass('color:red')
+divElement2.addStyle('color:green')
+console.log(divElement2)
